27ae0b60499293290d64d995b98d120fe4a00138
Chetan Chauhan <63583844+chetan1987chd@users.noreply.github.com>
2023-06-09 11:58:42 +1000

    Checking for mounted components

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { useState, useEffect } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
@@ -6,24 +7,34 @@ export default function useFetch(url) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const isMounted = useRef(false);
 
   useEffect(() => {
+    //this is to check if api is taking time to load on a page and we quickly navigate to other page
+    //then this will contine and cause error and may cause memory leak
+    isMounted.current = true;
+
     async function init() {
       try {
         const response = await fetch(baseUrl + url);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+
+          if (isMounted.current) setData(json);
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e);
+        if (isMounted.current) setError(e);
       } finally {
-        setLoading(false);
+        if (isMounted.current) setLoading(false);
       }
     }
     init();
+
+    return () => {
+      isMounted.current = false;
+    };
   }, [url]);
 
   return { data, error, loading };
